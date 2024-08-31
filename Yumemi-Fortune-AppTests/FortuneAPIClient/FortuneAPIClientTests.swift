07a6a3ba1a34ca0e3c5e016f5f85e06ad751e5f4
Masaki-JP <masakidayo0508@gmail.com>
2024-08-31 16:34:46 +0900

    chore: 可読性を向上させるため、setUpWithErrorをファイル下部にextentionとして分離

--- Yumemi-Fortune-AppTests/FortuneAPIClient/FortuneAPIClientTests.swift
+++ Yumemi-Fortune-AppTests/FortuneAPIClient/FortuneAPIClientTests.swift
@@ -5,21 +5,6 @@ final class FortuneAPIClientTests: XCTestCase {
 
     var fortuneAPIClient: FortuneAPIClient!
 
-    override func setUpWithError() throws {
-        let fortuneAPIResponseSample = FortuneAPIResponse.sample
-
-        guard let url = URL(string: "https://ios-junior-engineer-codecheck.yumemi.jp/my_fortune"),
-              let data = try? JSONEncoder().encode(fortuneAPIResponseSample),
-              let response: HTTPURLResponse = .init(url: url, statusCode: 200, httpVersion: nil, headerFields: nil) else {
-            XCTFail("An issue unrelated to the test subject has occurred."); return;
-        }
-
-        var targets: [URL: (error: Swift.Error?, data: Data?, response: HTTPURLResponse?)] = .init()
-        targets[url] = (error: nil, data: data, response: response)
-
-        fortuneAPIClient = .init(.mock(for: .fortuneAPI(targets)))
-    }
-
     func test_正常系_有効な引数が与えられた時に問題なく動作すること() async {
         /// Setup
         guard let birthday = try? Day(year: 2020, month: 1, day: 1) else {
@@ -36,5 +21,22 @@ final class FortuneAPIClientTests: XCTestCase {
     func test_異常系_空文字が与えられた時に適切なエラーを投げること() async {
 
     }
+}
 
+extension FortuneAPIClientTests {
+
+    override func setUpWithError() throws {
+        let fortuneAPIResponseSample = FortuneAPIResponse.sample
+
+        guard let url = URL(string: "https://ios-junior-engineer-codecheck.yumemi.jp/my_fortune"),
+              let data = try? JSONEncoder().encode(fortuneAPIResponseSample),
+              let response: HTTPURLResponse = .init(url: url, statusCode: 200, httpVersion: nil, headerFields: nil) else {
+            XCTFail("An issue unrelated to the test subject has occurred."); return;
+        }
+
+        var targets: [URL: (error: Swift.Error?, data: Data?, response: HTTPURLResponse?)] = .init()
+        targets[url] = (error: nil, data: data, response: response)
+
+        fortuneAPIClient = .init(.mock(for: .fortuneAPI(targets)))
+    }
 }
