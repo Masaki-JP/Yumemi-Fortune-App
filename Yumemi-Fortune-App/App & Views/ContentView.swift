4111e457facd78ff6ae4aee627758afdcb3ab143
Masaki-JP <masakidayo0508@gmail.com>
2024-09-03 15:09:16 +0900

    improve: Observationを用いてContentViewModelを再実装

--- Yumemi-Fortune-App/App & Views/ContentView.swift
+++ Yumemi-Fortune-App/App & Views/ContentView.swift
@@ -2,8 +2,7 @@ import SwiftUI
 
 @MainActor struct ContentView: View {
 
-struct ContentView: View {
-    @StateObject private var viewModel = ContentViewModel()
+    @State private var viewModel = ContentViewModel()
     @Environment(\.scenePhase) private var scenePhase
     @Environment(\.colorScheme) private var colorScheme
     @FocusState private var isFocusedNameTextField
