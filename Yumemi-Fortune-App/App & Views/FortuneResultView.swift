249dc43c24d7fe57fb8bfafae88b20a9d7f84f9f
Masaki-JP <masakidayo0508@gmail.com>
2024-09-05 02:35:57 +0900

    refactor: FortuneResultViewのコンテンツをRowとして切り出し

--- Yumemi-Fortune-App/App & Views/FortuneResultView.swift
+++ Yumemi-Fortune-App/App & Views/FortuneResultView.swift
@@ -9,50 +9,74 @@ struct FortuneResultView: View {
 
     var body: some View {
         List {
-            HStack(spacing: 0) {
-                Text("相性のいい都道府県：")
-                Spacer()
-                Text(fortuneAPIResponse.name)
-            }
-            HStack(spacing: 0) {
-                Text("首都：")
-                Spacer()
-                Text(fortuneAPIResponse.capital)
-            }
-            if let citizenDay = fortuneAPIResponse.citizenDay {
-                HStack(spacing: 0) {
-                    Text("市民の日：")
-                    Spacer()
-                    Text(citizenDay.month.description + "月")
-                    + Text(citizenDay.day.description + "日")
-                }
-            }
-            HStack(spacing: 0) {
-                Text("内陸県：")
-                Spacer()
-                Text(fortuneAPIResponse.hasCoastLine == false ? "はい" : "いいえ")
-            }
-            Text(fortuneAPIResponse.brief)
-            AsyncImage(
-                url: fortuneAPIResponse.logoURL,
-                transaction: .init(animation: .bouncy)
-            ) { asyncImagePhase in
-                switch asyncImagePhase {
-                case .empty:
-                    ProgressView()
-                case .success(let image):
-                    image
-                        .resizable()
-                        .scaledToFit()
-                case .failure(let error):
-                    Text(error.localizedDescription)
-                @unknown default:
-                    fatalError()
-                }
-            }
-            .frame(maxWidth: .infinity)
+            prefectureRow
+            capitalRow
+            citizenDayRow
+            hasCoastLineRow
+            logoImageRow
+                .frame(maxWidth: .infinity)
         }
         .navigationTitle("占い結果")
+    }
+
+    var prefectureRow: some View {
+        HStack(spacing: 0) {
+            Text("相性のいい都道府県：")
+            Spacer()
+            Text(fortuneAPIResponse.name)
+        }
+    }
+
+    var capitalRow: some View {
+        HStack(spacing: 0) {
+            Text("首都：")
+            Spacer()
+            Text(fortuneAPIResponse.capital)
+        }
+    }
+
+    @ViewBuilder
+    var citizenDayRow: some View {
+        if let citizenDay = fortuneAPIResponse.citizenDay {
+            HStack(spacing: 0) {
+                Text("市民の日：")
+                Spacer()
+                Text(citizenDay.month.description + "月")
+                + Text(citizenDay.day.description + "日")
+            }
+        }
+    }
+
+    var hasCoastLineRow: some View {
+        HStack(spacing: 0) {
+            Text("内陸県：")
+            Spacer()
+            Text(fortuneAPIResponse.hasCoastLine == false ? "はい" : "いいえ")
+        }
+    }
+
+    var briefRow: some View {
+        Text(fortuneAPIResponse.brief)
+    }
+
+    var logoImageRow: some View {
+        AsyncImage(
+            url: fortuneAPIResponse.logoURL,
+            transaction: .init(animation: .bouncy)
+        ) { asyncImagePhase in
+            switch asyncImagePhase {
+            case .empty:
+                ProgressView()
+            case .success(let image):
+                image
+                    .resizable()
+                    .scaledToFit()
+            case .failure(let error):
+                Text(error.localizedDescription)
+            @unknown default:
+                fatalError()
+            }
+        }
     }
 }
 
