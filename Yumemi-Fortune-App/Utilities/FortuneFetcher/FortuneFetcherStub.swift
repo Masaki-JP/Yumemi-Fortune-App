50d90fe3b198c6f069ffc5d072bff0db0a056390
Masaki-JP <masakidayo0508@gmail.com>
2024-09-05 03:01:20 +0900

    chore: FortuneFetcherStubのfetchResultをresultに改名

--- Yumemi-Fortune-App/Utilities/FortuneFetcher/FortuneFetcherStub.swift
+++ Yumemi-Fortune-App/Utilities/FortuneFetcher/FortuneFetcherStub.swift
@@ -1,14 +1,14 @@
 import Foundation
 
 struct FortuneFetcherStub: FortuneFetcherProtocol {
-    var fetchResult: Result<FortuneAPIResponse, FortuneFetcher.Error>
+    var result: Result<FortuneAPIResponse, FortuneFetcher.Error>
 
-    init(_ fetchResult: Result<FortuneAPIResponse, FortuneFetcher.Error>) {
-        self.fetchResult = fetchResult
+    init(_ result: Result<FortuneAPIResponse, FortuneFetcher.Error>) {
+        self.result = result
     }
 
     func fetch(name: String, birthday: Day, bloodType: BloodType) async throws -> FortuneAPIResponse {
-        switch fetchResult {
+        switch result {
         case .success(let value): return value
         case .failure(let error): throw error
         }
