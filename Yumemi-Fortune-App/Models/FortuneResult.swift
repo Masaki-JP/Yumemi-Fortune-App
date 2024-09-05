774da51cd819698f9647f7cd9a21477b8ba6e816
Masaki-JP <masakidayo0508@gmail.com>
2024-09-05 19:16:12 +0900

    doc: FortuneResultのドキュメントを作成

--- Yumemi-Fortune-App/Models/FortuneResult.swift
+++ Yumemi-Fortune-App/Models/FortuneResult.swift
@@ -1,13 +1,21 @@
 import Foundation
 
+/// 占い結果を表すオブジェクト。
 struct FortuneResult: Codable, Hashable {
+    /// 相性のいい都道府県を表す。
     let compatiblePrefectureName: String
+    /// 相性のいい都道府県の首都を表す。
     let capital: String
+    /// 相性のいい都道府県の県民日を表す。県民日が制定されていない都道府県の場合は`nil`になる。
     let citizenDay: CitizenDay?
+    /// 海岸の有無を表す。
     let hasCoastLine: Bool
+    /// 相性のいい都道府県の画像のリンク。
     let logoURL: URL
+    /// 相性のいい都道府県の概要。
     let brief: String
-
+    
+    /// 県民日を表すオブジェクト。
     struct CitizenDay: Codable, Hashable {
         let month, day: Int
     }
