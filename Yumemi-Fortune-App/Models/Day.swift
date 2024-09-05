db6115e73395b7357d0d39a040528766d63ca538
Masaki-JP <masakidayo0508@gmail.com>
2024-09-05 19:35:08 +0900

    doc: Dayのドキュメントを作成

--- Yumemi-Fortune-App/Models/Day.swift
+++ Yumemi-Fortune-App/Models/Day.swift
@@ -1,22 +1,37 @@
 import Foundation
 
+/// 日付を表すオプジェクト。
 struct Day: Codable, Comparable {
 
     let year, month, day: Int
 
+    /// 初期化時に発生することのあるエラー。
+    ///
     enum initializeError: Swift.Error {
         case invalidDay
         case invalidDecodedDay
         case unexpectedError(_ msg: String)
     }
-
+    
+    /// ``Day``のイニシャライザ。
+    ///
+    /// 2020年20月20日など存在しない日付は作成することができない。
+    /// このイニシャライザが投げる可能性のあるエラーは現状``Day/initializeError``のみである。
+    ///
     init(year: Int, month: Int, day: Int) throws {
         self.year = year
         self.month = month
         self.day = day
         try self.validate()
     }
-
+    
+    /// ``Day``のイニシャライザ。
+    ///
+    /// ``Foundation/Date``から``Day``を生成する。
+    /// このイニシャライザが投げる可能性のあるエラーは現状``Day/initializeError``のみである。
+    ///
+    /// - Parameter date: ``Day``に変換する``Foundation/Date``。
+    ///
     init(_ date: Date) {
         let year = Self.calendar.component(.year, from: date)
         let month = Self.calendar.component(.month, from: date)
@@ -46,7 +61,8 @@ struct Day: Codable, Comparable {
         calendar.timeZone = .init(identifier: "Asia/Tokyo")!
         return calendar
     }()
-
+    
+    /// アクセス時の``Day``を返す静的計算型プロパティ。
     static var today: Self {
         let now = Date.now
         let year = calendar.component(.year, from: now)
