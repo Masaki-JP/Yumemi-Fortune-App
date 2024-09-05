c08e40299d27cd41b5968e335d74f1864d0f3885
Masaki-JP <masakidayo0508@gmail.com>
2024-09-05 20:56:33 +0900

    doc: BloodTypeのドキュメントを作成

--- Yumemi-Fortune-App/Models/BloodType.swift
+++ Yumemi-Fortune-App/Models/BloodType.swift
@@ -1,5 +1,12 @@
 import Foundation
 
+/// 血液型を表すケース群。
+///
+/// ローバリューとして``Foundation/String``が設定されており、各ケースの`rawValue`はそのケースの大文字となる。
+///
+/// >note:
+/// エンコード時は小文字にエンコードされる。
+///
 enum BloodType: String, Codable, CaseIterable {
     case a = "A", b = "B", ab = "AB", o = "O"
 
