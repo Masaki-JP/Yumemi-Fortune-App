e510b924542272864af740f2f2f09200987d7a1b
Masaki-JP <masakidayo0508@gmail.com>
2024-09-13 22:20:21 +0900

    add: Userモデルを追加

---
+++ Yumemi-Fortune-App/Models/User.swift
@@ -0,0 +1,37 @@
+import SwiftData
+
+@Model
+final class User {
+    private(set) var name: String
+    private(set) var birthday: Day
+    private(set) var bloodType: BloodType
+    private(set) var fortuneResultList: [Day: FortuneResult]
+
+    var todayFortune: FortuneResult? {
+        fortuneResultList.first { $0.key == .today }?.value
+    }
+
+    init(name: String, birthday: Day, bloodType: BloodType, fortuneResults: [Day: FortuneResult] = .init()) {
+        self.name = name
+        self.birthday = birthday
+        self.bloodType = bloodType
+        self.fortuneResultList = fortuneResults
+    }
+
+    func updateName(to name: String) {
+        self.name = name
+    }
+
+    func updateBirthday(to birthday: Day) {
+        self.birthday = birthday
+    }
+
+    func updateBloodType(to bloodType: BloodType) {
+        self.bloodType = bloodType
+    }
+
+    func addFortuneResult(_ fortuneResult: FortuneResult) {
+        guard fortuneResultList[.today] == nil else { return }
+        fortuneResultList[.today] = fortuneResult
+    }
+}
