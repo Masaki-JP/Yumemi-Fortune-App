036234de9edfeaa1f2a152aa8a2d261866b3f3ca
Masaki-JP <masakidayo0508@gmail.com>
2024-09-13 22:26:10 +0900

    feat: FortuneResultの静的な計算型プロパティsample2を追加

--- Yumemi-Fortune-App/Extentions/FortuneResult+.swift
+++ Yumemi-Fortune-App/Extentions/FortuneResult+.swift
@@ -20,4 +20,23 @@ extension FortuneResult {
             brief: "静岡県（しずおかけん）は、日本の中部地方に位置する県。県庁所在地は静岡市。\n※出典: フリー百科事典『ウィキペディア（Wikipedia）』"
         )
     }
+
+    /// ``FortuneResult``のサンプル。
+    ///
+    /// - 都道府県：静岡県
+    /// - 首都：静岡市
+    /// - 県民日：8月21日
+    /// - 海岸の有無：有り
+    /// - ロゴURL：[https://japan-map.com/wp-content/uploads/shizuoka.png](https://japan-map.com/wp-content/uploads/shizuoka.png)
+    ///
+    static var sample2: Self {
+        .init(
+            compatiblePrefecture: "兵庫県",
+            capital: "神戸市",
+            citizenDay: nil,
+            hasCoastLine: true,
+            logoURL: URL(string: "https://japan-map.com/wp-content/uploads/hyogo.png")!,
+            brief: "兵庫県（ひょうごけん）は、日本の近畿地方の西端に位置する県。県庁所在地は神戸市。\n※出典: フリー百科事典『ウィキペディア（Wikipedia）』"
+        )
+    }
 }
