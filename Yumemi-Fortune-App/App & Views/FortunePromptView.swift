ac67e93d16c003241c08bbdfcbf163fd9d2bd75c
Masaki-JP <masakidayo0508@gmail.com>
2024-09-14 18:19:39 +0900

    add: FortunePromptViewを追加

---
+++ Yumemi-Fortune-App/App & Views/FortunePromptView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+func fortunePromptView(action: @escaping () -> Void) -> some View {
+    ContentUnavailableView {
+        Label("今日のラッキー都道府県を\n占いましょう！", systemImage: "map.fill")
+    } actions: {
+        Button("占う", action: action)
+            .buttonStyle(.borderedProminent)
+    }
+}
+
+#Preview {
+    fortunePromptView(action: {})
+}
