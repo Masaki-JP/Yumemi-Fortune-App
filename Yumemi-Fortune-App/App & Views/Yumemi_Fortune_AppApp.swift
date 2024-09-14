76364ad51f3586570199f0cf17abe012f25e1210
Masaki-JP <masakidayo0508@gmail.com>
2024-09-14 17:11:39 +0900

    feat: エントリポイントにsharedModelContainerを追加（注意点あり）
    sharedModelContainerを追加しておかなければ、SwiftDataを用いたViewのプレビューがクラッシュするので、事前に追加しておく。

--- Yumemi-Fortune-App/App & Views/Yumemi_Fortune_AppApp.swift
+++ Yumemi-Fortune-App/App & Views/Yumemi_Fortune_AppApp.swift
@@ -1,7 +1,19 @@
 import SwiftUI
+import SwiftData
 
 @main
 struct Yumemi_Fortune_AppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([User.self])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
     var body: some Scene {
         WindowGroup {
             Text("Hello, world.")
