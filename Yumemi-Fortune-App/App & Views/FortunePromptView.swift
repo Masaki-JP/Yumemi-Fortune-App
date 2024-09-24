0bebcf291c9d791eb402cdab1d038417b7f55eae
Masaki-JP <masakidayo0508@gmail.com>
2024-09-25 06:51:02 +0900

    fix: fortunePromptViewメソッドに@MainActorを付与

--- Yumemi-Fortune-App/App & Views/FortunePromptView.swift
+++ Yumemi-Fortune-App/App & Views/FortunePromptView.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 
+@MainActor
 func fortunePromptView(action: @escaping () -> Void) -> some View {
     ContentUnavailableView {
         Label("今日のラッキー都道府県を\n占いましょう！", systemImage: "map.fill")
