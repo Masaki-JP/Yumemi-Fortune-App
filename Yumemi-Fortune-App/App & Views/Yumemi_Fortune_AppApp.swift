f673471ce336150f6c34e8813163d118b169c840
Masaki-JP <masakidayo0508@gmail.com>
2024-09-29 22:59:11 +0900

    change: SwiftDataのオートセーブを無効化

--- Yumemi-Fortune-App/App & Views/Yumemi_Fortune_AppApp.swift
+++ Yumemi-Fortune-App/App & Views/Yumemi_Fortune_AppApp.swift
@@ -3,21 +3,10 @@ import SwiftData
 
 @main
 struct Yumemi_Fortune_AppApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([User.self])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
     var body: some Scene {
         WindowGroup {
             RootView()
-                .modelContainer(sharedModelContainer)
+                .modelContainer(for: User.self, isAutosaveEnabled: false)
         }
     }
 }
