d4dbbad8450d7f374160dc76ffb392d70e61566e
Masaki-JP <masakidayo0508@gmail.com>
2024-09-30 01:07:09 +0900

    chore: SettingListViewからNameEditViewを切り出し

--- Yumemi-Fortune-App/App & Views/SettingListView.swift
+++ Yumemi-Fortune-App/App & Views/SettingListView.swift
@@ -78,52 +78,6 @@ struct SettingListView: View {
     }
 }
 
-@MainActor
-struct NameEditView: View {
-    private let originName: String
-    private let didTapSaveButton: (_ newName: String, _ dismissAction: (() -> Void)?) -> Void
-
-    @State private var newName: String
-    @Environment(\.dismiss) private var dismiss
-
-    init(
-        originName: String,
-        didTapSaveButton: @escaping (_ newName: String, _ dismissAction: (() -> Void)?) -> Void
-    ) {
-        self.originName = originName
-        self.didTapSaveButton = didTapSaveButton
-
-        self._newName = .init(wrappedValue: originName)
-    }
-
-    var body: some View {
-        Form {
-            Section {
-                HStack(spacing: 0) {
-                    TextField("例：田中　太郎", text: $newName)
-                    Spacer()
-                    Image(systemName: "x.circle.fill")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 20)
-                        .foregroundStyle(.gray)
-                        .onTapGesture { newName.removeAll() }
-                }
-            } header: {
-                Text("新しい名前を入力")
-            }
-        }
-        .toolbar {
-            ToolbarItem(placement: .topBarTrailing) {
-                Button("保存") {
-                    didTapSaveButton(newName, dismiss.callAsFunction)
-                }
-                .disabled(newName.isEmpty || originName == newName)
-            }
-        }
-    }
-}
-
 private struct SettingListViewWrapper: View {
     @Environment(\.modelContext) private var modelContext
     let user = User(name: "Naruto", birthday: .today, bloodType: .a)
