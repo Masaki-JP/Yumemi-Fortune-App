84be2f2536b80e68ad8b608fa43af3beb58f8b81
Masaki-JP <masakidayo0508@gmail.com>
2024-09-30 21:01:05 +0900

    chore: FortuneResultにsample3を追加

--- Yumemi-Fortune-App/Extentions/FortuneResult+.swift
+++ Yumemi-Fortune-App/Extentions/FortuneResult+.swift
@@ -39,4 +39,23 @@ extension FortuneResult {
             brief: "兵庫県（ひょうごけん）は、日本の近畿地方の西端に位置する県。県庁所在地は神戸市。\n※出典: フリー百科事典『ウィキペディア（Wikipedia）』"
         )
     }
+
+    /// ``FortuneResult``のサンプル。
+    ///
+    /// - 都道府県：千葉県
+    /// - 首都：千葉市
+    /// - 県民日：6月15日
+    /// - 海岸の有無：有り
+    /// - ロゴURL：[https://japan-map.com/wp-content/uploads/chiba.png](https://japan-map.com/wp-content/uploads/chiba.png)
+    ///
+    static var sample3: Self {
+        .init(
+            compatiblePrefecture: "千葉県",
+            capital: "千葉市",
+            citizenDay: .init(month: 6, day: 15),
+            hasCoastLine: true,
+            logoURL: URL(string: "https://japan-map.com/wp-content/uploads/chiba.png")!,
+            brief: "千葉県（ちばけん）は、日本の関東地方に位置する県。県庁所在地は千葉市。\n首都圏を構成し、都道府県人口・人口密度は第6位、県の財政力指数は全国第5位、面積は第28位の規模である。\n※出典: フリー百科事典『ウィキペディア（Wikipedia）』"
+        )
+    }
 }
