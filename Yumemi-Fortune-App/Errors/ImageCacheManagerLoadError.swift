e20b92ae436700239d218b62e3e99f1a3fc9db57
Masaki-JP <masakidayo0508@gmail.com>
2024-10-06 00:10:51 +0900

    doc: ImageCacheManagerSaveErrorとImageCacheManagerLoadErrorにドキュメントを追加

--- Yumemi-Fortune-App/Errors/ImageCacheManagerLoadError.swift
+++ Yumemi-Fortune-App/Errors/ImageCacheManagerLoadError.swift
@@ -1,5 +1,7 @@
 import Foundation
 
+/// ``ImageCacheManager/load(with:)``メソッドの実行時に発生することのあるエラー。
+///
 enum ImageCacheManagerLoadError: Error {
     case cacheDirectoryNotFound
     case multipleCacheDirectoriesFound
--- Yumemi-Fortune-App/Errors/ImageCacheManagerSaveError.swift
+++ Yumemi-Fortune-App/Errors/ImageCacheManagerSaveError.swift
@@ -1,5 +1,7 @@
 import Foundation
 
+/// ``ImageCacheManager/save(data:webResourceURL:)``メソッドの実行時に発生することのあるエラー。
+///
 enum ImageCacheManagerSaveError: Error {
     case cacheDirectoryNotFound
     case multipleCacheDirectoriesFound
