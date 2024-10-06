34c70de8589fc4949dafbe4715d10c0f6d797309
Masaki-JP <masakidayo0508@gmail.com>
2024-10-06 23:03:17 +0900

    doc: FortuneResultViewModelにドキュメントを追加

--- Yumemi-Fortune-App/ViewModels/FortuneResultViewModel.swift
+++ Yumemi-Fortune-App/ViewModels/FortuneResultViewModel.swift
@@ -1,11 +1,24 @@
 import SwiftUI
 
+/// ``FortuneResultView``で使用するViewModel。
+///
+/// 何か問題が生じた際はアラートを使わず、``getPrefectureImageResult``を通してユーザーに伝える。
+///
 @MainActor @Observable
 final class FortuneResultViewModel {
 
+    /// ``FortuneResultView``で詳細を表示する``FortuneResult``。
+    ///
     let fortuneResult: FortuneResult
+
+    /// ``FortuneResultView``に都道府県の画像、もしくはエラーを伝えるためのプロパティ。
+    ///
+    /// >note: 画像の取得中は`nil`としている。
+    ///
     private(set) var getPrefectureImageResult: Result<UIImage, GetPrefectureImageError>? = nil
 
+    /// `Result<Success, Failure>`を使用するために作成したオブジェクト。
+    ///
     struct GetPrefectureImageError: Error {
         let message: String
 
@@ -18,15 +31,23 @@ final class FortuneResultViewModel {
         self.fortuneResult = fortuneResult
     }
 
+    /// ``FortuneResultView``が表示された時に実行される処理。
+    ///
+    /// `task`モディファイア内で呼ばれることを想定している。
+    ///
     func onAppearAction() async {
+        /// すでに成功している場合は早期リターンする。
         if case .success = getPrefectureImageResult { return }
+        /// 失敗していた場合を考慮し、`nil`を代入する。`.failure`のままだと`ProgressView`が表示されない。
         getPrefectureImageResult = nil
 
+        /// キャッシュから画像データの取得し`UIImage`を生成できた場合、`getPrefectureImageResult`に成功として代入する。
         if let prefectureImageData = await getPrefectureImageData(),
            let uiImage = UIImage(data: prefectureImageData) {
             getPrefectureImageResult = .success(uiImage); return
         }
 
+        /// 通信を用いて画像データを取得する。失敗した場合は、エラーを`getPrefectureImageResult`に失敗として代入する。
         let prefectureImageData: Data
         switch await fetchPrefectureImageData() {
         case .success(let data):
@@ -35,19 +56,25 @@ final class FortuneResultViewModel {
             getPrefectureImageResult = .failure(error); return
         }
 
+        /// 取得した画像データから`UIImage`を生成する。失敗した場合は、`getPrefectureImageResult`に`GetPrefectureImageError`を失敗として代入する。
         guard let uiImage = UIImage(data: prefectureImageData) else {
             getPrefectureImageResult = .failure(.init("予期せぬエラーが発生しました。")); return
         }
 
+        /// 生成した`UIImage`を`getPrefectureImageResult`に成功としてに代入する。
         getPrefectureImageResult = .success(uiImage)
 
+        /// 画像が保存されているURL（Webリソース）をキーとして、取得したデータをセーブする。
         try? await ImageCacheManager.shared.save(data: prefectureImageData, webResourceURL: fortuneResult.logoURL)
     }
 }
 
-extension FortuneResultViewModel {
+// MARK: - The following methods are private.
 
-    private func getPrefectureImageData() async -> Data? {
+private extension FortuneResultViewModel {
+
+    /// キャッシュから画像データを取得する。
+    func getPrefectureImageData() async -> Data? {
         do throws(ImageCacheManagerLoadError) {
             return try await ImageCacheManager.shared.load(with: fortuneResult.logoURL)
         } catch {
@@ -58,7 +85,8 @@ extension FortuneResultViewModel {
         }
     }
 
-    private func fetchPrefectureImageData() async -> Result<Data, GetPrefectureImageError> {
+    /// 通信を用いて画像データを取得する。
+    func fetchPrefectureImageData() async -> Result<Data, GetPrefectureImageError> {
         do {
             let (data, _) = try await URLSession.shared.data(from: fortuneResult.logoURL)
             return .success(data)
@@ -74,7 +102,7 @@ extension FortuneResultViewModel {
             default: nil
             }
 
-            return .failure(message == nil ? .init() : .init(message!))
+            return .failure(message == nil ? .init() : .init(message!)) // Force-Unwrap
         } catch {
             return .failure(.init())
         }
